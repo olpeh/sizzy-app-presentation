24321cb190968387614a2b33c9d3523656b3d4e3
Olavi Haapala <olavi.haapala@futurice.com>
2019-08-23 09:59:03 +0300

    Updates

---
+++ precache-manifest.0e6446cc22cc95203e95fffdb4547aaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3ff13317fd56041d4613d8cd6ca201a",
+    "url": "/sizzy-app-presentation/index.html"
+  },
+  {
+    "revision": "97d3b8cc0e2ddc3c5795",
+    "url": "/sizzy-app-presentation/static/css/main.74323ce9.chunk.css"
+  },
+  {
+    "revision": "7267d26204585dbe24fb",
+    "url": "/sizzy-app-presentation/static/js/2.f39e9b42.chunk.js"
+  },
+  {
+    "revision": "97d3b8cc0e2ddc3c5795",
+    "url": "/sizzy-app-presentation/static/js/main.6853d203.chunk.js"
+  },
+  {
+    "revision": "991f6bc53fe52260ac10",
+    "url": "/sizzy-app-presentation/static/js/runtime~main.28d50185.js"
+  },
+  {
+    "revision": "aac76427e6479e98c1064aaf2182c27a",
+    "url": "/sizzy-app-presentation/static/media/logo.aac76427.svg"
+  }
+]);
